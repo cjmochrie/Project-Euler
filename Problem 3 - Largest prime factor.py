262576acb6dc4861468318e36641bec3e350e4dd
cjmochrie <cameron.mochrie@gmail.com>
2015-06-17 09:28:45 -0400

    Solve largest prime factor

---
+++ Problem 3 - Largest prime factor.py
@@ -0,0 +1,30 @@
+import math
+
+def isPrime(num):
+
+	if num < 4:
+		return True
+	else:
+		i = 2
+		while i < math.sqrt(num):
+			if num % i == 0:
+				return False
+			i += 1
+
+	return True
+
+def primeFactors(num):
+	if isPrime(num):
+		return num
+	i = 2
+	while i < num:
+		if num % i == 0:
+			if isPrime(i):
+				return i, primeFactors(num/i)
+		i += 1
+
+
+print primeFactors(600851475143)
+
+
+
