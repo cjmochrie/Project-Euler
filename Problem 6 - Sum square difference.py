1453304f7f69a73c70b0771df3c5bfd80f80db43
cjmochrie <cameron.mochrie@gmail.com>
2015-06-18 15:01:49 -0400

    Solve problem 6

---
+++ Problem 6 - Sum square difference.py
@@ -0,0 +1,20 @@
+square_of_sum = 0
+sum = 0
+for i in range(1,101):
+	sum += i
+
+square_of_sum = sum*sum
+
+
+
+
+# sum of squares
+sum = 0
+for i in range(1,101):
+	print i
+	sum += i*i
+
+print square_of_sum
+print sum
+print square_of_sum - sum
+
