d67d67164b615fe753d61c5a656a62121bd0318e
cjmochrie <cameron.mochrie@gmail.com>
2015-06-20 19:42:28 -0400

    Refactored Sum of Primes, faster by ~factor of 5

--- Problem 10 - Sum of Primes.py
+++ Problem 10 - Sum of Primes.py
@@ -1,24 +1,27 @@
 import math
+from timed_function import timer
 
-def isPrime(num):
 
-	if num < 4:
-		return True
-	else:
-		if num % 2 == 0:
-			return False
-		else:
-			i = 3
-			while i <= math.sqrt(num):
-				if num % i == 0:
-					return False
-				i += 2
+@timer
+def sum_primes():
+	primes =[2]
+	sum = 2
+	is_prime = True
+	for i in xrange(3,2000000,2):
+		sqrt_i = math.sqrt(i)
+		for prime in primes:
+			if i % prime == 0:
+				is_prime = False
+				break
+			if prime > sqrt_i:
+				break
+		if is_prime:
+			primes.append(i)
+			sum += i
+		is_prime = True
 
-	return True
 
-sum = 2
-for i in xrange(3,2000000,2):
-	if isPrime(i):
-		sum += i
+	print sum
+	#print primes
 
-print sum
+sum_primes()
